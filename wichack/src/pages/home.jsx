5e27e0866711cfdd77eb5d59e2a7917e67dcd659
Netra Krishnan <netra.kris@gmail.com>
2023-03-04 13:30:50 -0500

    dsfsd

--- wichack/src/pages/home.jsx
+++ wichack/src/pages/home.jsx
@@ -1,13 +1,12 @@
 
 import React from 'react';
-
+import './home.css';
 
 function Home() {
 
   return (
-    <div>
-     <h1>Hello World
-        </h1>
+    <div className='Home'>
+     jdbfldskjbvdkjzvnkxdn
 
     </div>
   )
